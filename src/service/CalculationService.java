b5bc7b553ea42e14d050221805c8c38ac017a6d7
rodrigodojo <rodrigodoj@gmail.com>
2021-02-11 11:18:41 -0300

    Exemplo de generics usando <Type>

--- src/service/CalculationService.java
+++ src/service/CalculationService.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class CalculationService {
 	
-	public static Integer max(List<Integer> list) {
+	public static <Type extends Comparable<Type>> Type max(List<Type> list) {
 		if(list.isEmpty()) {
 			throw new IllegalStateException("List can't be empty");
 		}
-		Integer max = list.get(0);
-		for(Integer item : list) {
+		Type max = list.get(0);
+		for(Type item : list) {
 			if(item.compareTo(max) > 0) {
 				max = item ;
 			}
